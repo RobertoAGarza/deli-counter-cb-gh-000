eb4a5905623d4f0d9e73a086871dcbf5a0aee346
Roberto Garza <robertg4567@hotmail.com>
2018-07-19 17:23:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(lineArray)
+  myString = "The line is currently: "
+  if lineArray.empty? 
+    puts "The line is currently empty."
+  elsif lineArray.size != 0
+    i = 1
+    lineArray.each do |customer|
+      myString = myString + "#{i}. #{lineArray[i-1]} "
+      i+=1
+    end
+  end
+  puts "#{myString}"
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving(array)
+
+
+end
